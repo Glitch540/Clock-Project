d71c327fdbc8bf0098e60ff50457b01e30ee323f
Glitch540 <marbz1234walkerz@gmail.com>
2024-11-16 00:08:54 -0800

    Final
    complete

--- src/index.js
+++ src/index.js
@@ -47,10 +47,6 @@
 }
 
 
-
-
-
-
  }
 
 function updateCity(event) {
@@ -62,16 +58,14 @@ function updateCity(event) {
    let cityTime = moment().tz(cityTimeZone);
    let citiesElement = document.querySelector("#cities");
    citiesElement.innerHTML = `
+   <div id="cities">
    <div class="city">
-     <div>
        <h2>${cityName}</h2>
+      <div class="details">
        <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
-     </div>
-     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-     "A"
-   )}</small></div>
+     <div class="time">${cityTime.format("h:mm:ss ss")} <small>${cityTime.format("A")}</small></div>
    </div>
-   `;
+   </div>`;
  }
  
  updateTime();
